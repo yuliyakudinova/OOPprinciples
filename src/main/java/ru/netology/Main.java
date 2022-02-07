8f4c5739e6856f734568655cd29c7f4121bd2059
yulechka.kudinova@mail.ru <yulechka.kudinova.1991@mail.ru>
2022-02-07 20:12:56 +0300

    second commit

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -3,8 +3,12 @@ package ru.netology;
 public class Main {
     public static void main(String[] args) {
         Post post = new Post();
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
         post.name = "Иван";
-        post.passport = "4444 \u2116 44444444";
+        post.passport = "4444 № 44444444";
         post.patronymic = "Иванович";
         post.phone = "+7 (999)-999-99-99";
         post.surname = "Иванов";
--- src/main/java/ru/netology/Post.java
+++ src/main/java/ru/netology/Post.java
@@ -7,5 +7,6 @@ public class Post {
     public String phone;
     public String surname;
     public boolean subscription;
+    public FormDate birthday;
 
 }
